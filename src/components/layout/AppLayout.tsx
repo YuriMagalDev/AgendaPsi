f8fc418669a6851bd06d5c04cc95f02dd0f92ad5
Yuri <yurisylex@gmail.com>
2026-04-17 07:33:24 -0300

    feat: integrate TopBar into AppLayout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import { Sidebar } from './Sidebar'
 import { BottomNav } from './BottomNav'
+import { TopBar } from './TopBar'
 
 export function AppLayout() {
   return (
     <div className="flex min-h-screen bg-bg">
       <Sidebar />
-      <main className="flex-1 overflow-auto pb-20 md:pb-0">
-        <Outlet />
-      </main>
+      <div className="flex flex-col flex-1 min-w-0">
+        <TopBar />
+        <main className="flex-1 overflow-auto pb-20 md:pb-0">
+          <Outlet />
+        </main>
+      </div>
       <BottomNav />
     </div>
   )
